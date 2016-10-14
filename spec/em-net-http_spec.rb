02618c1d17162df876ed24a4baf0b570f991dc50
James Fairbairn <james@netlagoon.com>
2016-10-13 21:18:13 -0700

    Added test for exception

--- spec/em-net-http_spec.rb
+++ spec/em-net-http_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'mimic'
 
 describe "em-net-http" do
   around(:each) do |example|
@@ -7,91 +8,137 @@ describe "em-net-http" do
     end.resume
   end
 
-  it 'should support streaming the response' do
-    assert_identical(true) {
-      body = StringIO.new '', 'wb'
-
-      Net::HTTP.start('localhost', Mimic::MIMIC_DEFAULT_PORT) do |http|
-        http.request_get "/image" do |resp|
-          resp.should be_a_kind_of(Net::HTTPOK)
-          resp.read_body { |chunk| body.write chunk }
-          resp
+  describe '' do
+    before(:all) do
+      Mimic.mimic do
+        Net::HTTPResponse::CODE_TO_OBJ.each do |code, klass|
+          get("/code/#{code}").returning("#{code} #{klass.name}", code.to_i, {})
         end
-      end.tap do |resp|
-        resp.instance_variable_set :@streamed_body, body.string
-      end
-    }
-  end
 
-  it 'should support buffering the response' do
-    assert_identical {
-      Net::HTTP.start('localhost', Mimic::MIMIC_DEFAULT_PORT) do |http|
-        respone = http.request_get "/image" do |resp|
-          resp.should be_a_kind_of(Net::HTTPOK)
-          resp.read_body # force reading the body before the test tears down the EM loop
-          resp
-        end
-        respone.tap { respone.should be_a_kind_of(Net::HTTPOK) }
-      end
-    }
-  end
+        get('/hello').returning('Hello World!', 200, {'Content-Type'=>'text/plain'})
 
-  describe 'should be compatible' do
-    it 'for Net::HTTP.get()' do
-      run_requests {Net::HTTP.get(URI.parse("http://localhost:#{Mimic::MIMIC_DEFAULT_PORT}/hello"))}
-      @expected_res.should == @actual_res
-    end
-
-    # it 'for Net::HTTP.get_print()' do
-    #   run_requests {Net::HTTP.get_print(URI.parse('http://localhost/hello'))}
-    #   @expected_res.should == @actual_res
-    # end
-
-    # We don't test responses like 100 Continue at the moment.
-    %w(200 201 202 203 204 205 206 300 301 302 303 307 400 401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 500 501 502 503 504 505).each do |code|
-      it "for Net::HTTP.start(host, port, &block) with response code #{code}" do
-        assert_identical {
-          Net::HTTP.start('localhost', Mimic::MIMIC_DEFAULT_PORT) do |http|
-            http.get("/code/#{code}").tap { |resp|
-              # Force the response to be buffered while we are still in the EM loop, since we shut it down EM before the verifications
-              resp.body
+        class BigImageResponse
+          def each
+            ::File.open('spec/image.jpg', "rb") { |file|
+              while part = file.read(8192)
+                yield part
+              end
             }
           end
-        }
+        end
+        resp = BigImageResponse.new
+        get('/image').returning(resp, 200, {"Content-Type" => 'image/jpeg'})
+
+        post('/testpost') do
+          "You said #{request.body.read}."
+        end
+      end
+    end
+
+    it 'should support streaming the response' do
+      assert_identical(true) {
+        body = StringIO.new '', 'wb'
+
+        Net::HTTP.start('localhost', Mimic::MIMIC_DEFAULT_PORT) do |http|
+          http.request_get "/image" do |resp|
+            resp.should be_a_kind_of(Net::HTTPOK)
+            resp.read_body { |chunk| body.write chunk }
+            resp
+          end
+        end.tap do |resp|
+          resp.instance_variable_set :@streamed_body, body.string
+        end
+      }
+    end
+
+    it 'should support buffering the response' do
+      assert_identical {
+        Net::HTTP.start('localhost', Mimic::MIMIC_DEFAULT_PORT) do |http|
+          respone = http.request_get "/image" do |resp|
+            resp.should be_a_kind_of(Net::HTTPOK)
+            resp.read_body # force reading the body before the test tears down the EM loop
+            resp
+          end
+          respone.tap { respone.should be_a_kind_of(Net::HTTPOK) }
+        end
+      }
+    end
+
+    describe 'should be compatible' do
+      it 'with Net::HTTP.get()' do
+        run_requests {Net::HTTP.get(URI.parse("http://localhost:#{Mimic::MIMIC_DEFAULT_PORT}/hello"))}
+        @expected_res.should == @actual_res
       end
 
-      it "for Net::HTTP.new(host, port).start(&block) with response code #{code}" do
+      # it 'for Net::HTTP.get_print()' do
+      #   run_requests {Net::HTTP.get_print(URI.parse('http://localhost/hello'))}
+      #   @expected_res.should == @actual_res
+      # end
+
+      # We don't test responses like 100 Continue at the moment.
+      %w(200 201 202 203 204 205 206 300 301 302 303 307 400 401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 500 501 502 503 504 505).each do |code|
+        it "with Net::HTTP.start(host, port, &block) with response code #{code}" do
+          assert_identical {
+            Net::HTTP.start('localhost', Mimic::MIMIC_DEFAULT_PORT) do |http|
+              http.get("/code/#{code}").tap { |resp|
+                # Force the response to be buffered while we are still in the EM loop, since we shut it down EM before the verifications
+                resp.body
+              }
+            end
+          }
+        end
+
+        it "with Net::HTTP.new(host, port).start(&block) with response code #{code}" do
+          assert_identical {
+            h = Net::HTTP.new('localhost', Mimic::MIMIC_DEFAULT_PORT)
+            h.start do |http|
+              http.get("/code/#{code}")
+            end
+          }
+        end
+      end
+
+      it "with response code 304" do
         assert_identical {
-          h = Net::HTTP.new('localhost', Mimic::MIMIC_DEFAULT_PORT)
-          h.start do |http|
-            http.get("/code/#{code}")
+          Net::HTTP.start('localhost', Mimic::MIMIC_DEFAULT_PORT) do |http|
+            req = Net::HTTP::Get.new('/code/304')
+            req['If-Modified-Since'] = Time.now.rfc2822
+            http.request(req)
           end
         }
+
       end
-    end
 
-    it "with response code 304" do
-      assert_identical {
-        Net::HTTP.start('localhost', Mimic::MIMIC_DEFAULT_PORT) do |http|
-          req = Net::HTTP::Get.new('/code/304')
-          req['If-Modified-Since'] = Time.now.rfc2822
-          http.request(req)
-        end
-      }
+      it 'with post' do
+        assert_identical {
+          Net::HTTP.start('localhost', Mimic::MIMIC_DEFAULT_PORT) do |http|
+            req = Net::HTTP::Post.new('/testpost')
+            req.body = 'hello mimic'
+            http.request(req)
+          end
+        }
 
-    end
-
-    it 'with post' do
-      assert_identical {
-        Net::HTTP.start('localhost', Mimic::MIMIC_DEFAULT_PORT) do |http|
-          req = Net::HTTP::Post.new('/testpost')
-          req.body = 'hello mimic'
-          http.request(req)
-        end
-      }
+      end
 
     end
 
+
+
+    after(:all) do
+      Mimic.cleanup!
+    end
+
+  end
+
+  it 'should raise relevant exceptions' do
+    begin
+      Net::HTTP.start('localhost', 1) do |r|
+        r.get('/')
+      end
+      fail('expected exception to be raised')
+    rescue => expected
+      expected.class.should == Errno::ECONNREFUSED
+    end
   end
 
   def run_requests(&block)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,37 +3,3 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'em-net-http'
 require 'time'
 require 'rspec'
-
-require 'mimic'
-
-RSpec.configure do |config|
-  config.before(:all) do
-    Mimic.mimic do
-      Net::HTTPResponse::CODE_TO_OBJ.each do |code, klass|
-        get("/code/#{code}").returning("#{code} #{klass.name}", code.to_i, {})
-      end
-      
-      get('/hello').returning('Hello World!', 200, {'Content-Type'=>'text/plain'})
-      
-      class BigImageResponse
-        def each
-          ::File.open('spec/image.jpg', "rb") { |file|
-            while part = file.read(8192)
-              yield part
-            end
-          }
-        end
-      end
-      resp = BigImageResponse.new
-      get('/image').returning(resp, 200, {"Content-Type" => 'image/jpeg'})
-
-      post('/testpost') do
-        "You said #{request.body.read}."
-      end
-    end
-  end
-  
-  config.after(:all) do
-    Mimic.cleanup!
-  end
-end
